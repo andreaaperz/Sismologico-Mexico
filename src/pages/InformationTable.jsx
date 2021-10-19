3fffa42a3a519fc909efe29bdcdb630e6c11943f
Gustavo <guga.nd@hotmail.com>
2021-10-18 20:42:54 -0500

    tabla con input lupa

--- src/pages/InformationTable.jsx
+++ src/pages/InformationTable.jsx
@@ -7,39 +7,52 @@ export default function InformationTable() {
    
  
   return (
-    
-    <Table responsive>
-          <thead>
-              <tr>
-                  <th>#</th>
-                  {Array.from({ length: 12 }).map((_, index) => (
-                      <th key={index}>Table heading</th>
-                  ))}
-              </tr>
-          </thead>
-          <tbody>
-              <tr>
-                  <td>1</td>
-                  {Array.from({ length: 12 }).map((_, index) => (
-                      <td key={index}>Table cell {index}</td>
-                  ))}
-              </tr>
-              <tr>
-                  <td>2</td>
-                  {Array.from({ length: 12 }).map((_, index) => (
-                      <td key={index}>Table cell {index}</td>
-                  ))}
-              </tr>
-              <tr>
-                  <td>3</td>
-                  {Array.from({ length: 12 }).map((_, index) => (
-                      <td key={index}>Table cell {index}</td>
-                  ))}
-              </tr>
-          </tbody>
-      </Table>
-  
+      <div className='table'>
+          <InputGroup className="mb-3">
+    <InputGroup.Text id="inputGroup-sizing-default"><img className='lupa' src="http://assets.stickpng.com/thumbs/5c790123003fa702a1d2795b.png" alt="lupa" srcset="https://cdn.pixabay.com/photo/2013/10/01/16/55/magnifying-glass-189254_640.png" /></InputGroup.Text>
+    <FormControl
+      aria-label="Default"
+      aria-describedby="inputGroup-sizing-default"
+    />
+  </InputGroup>
+  <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Lugar</th>
+      <th>Magnitud</th>
+      <th>Profundidad(km)</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Acapulco</td>
+      <td>7.1</td>
+      <td>2.0</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>San Felipe</td>
+      <td>4.0</td>
+      <td>11</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Cdmx</td>
+      <td>4.2</td>
+      <td>8</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>Puebla</td>
+      <td>4.8</td>
+      <td>7</td>
+    </tr>
+
+  </tbody>
+</Table>
+      </div>
   );
 }
 
-export default InformationTable;
